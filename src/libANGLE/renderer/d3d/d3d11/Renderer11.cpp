jrmuizel/angle

Bug 1079400. Limit the maximum render target size to avoid crashes. r=jgilbert
This avoids crashes with certain NVIDIA drivers.

src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -2474,6 +2474,12 @@ VendorID Renderer11::getVendorId() const
     return static_cast<VendorID>(mAdapterDescription.VendorId);
 }
 
+SIZE_T Renderer11::getMaxResourceSize() const
+{
+    // This formula comes from http://msdn.microsoft.com/en-us/library/windows/desktop/ff819065%28v=vs.85%29.aspx
+    return std::min(std::max(SIZE_T(128 * 1024 * 1024), mAdapterDescription.DedicatedVideoMemory), SIZE_T(2048) * 1024 * 1024);
+}
+
 std::string Renderer11::getRendererDescription() const
 {
     std::ostringstream rendererString;
@@ -2829,6 +2835,15 @@ void Renderer11::setOneTimeRenderTarget(ID3D11RenderTargetView *renderTargetView
     mAppliedDSV = DirtyPointer;
 }
 
+UINT64 EstimateSize(D3D11_TEXTURE2D_DESC &desc)
+{
+    //XXX: handle overflow (64 bits should be enough for anyone...)
+    const d3d11::DXGIFormat &dxgiFormatInfo = d3d11::GetDXGIFormatInfo(desc.Format);
+    // NVIDIA seems to align the width of buffers by 8 and the height by 64, so we do the same.
+    UINT64 total = UINT64(rx::roundUp(desc.Width, UINT(8))) * rx::roundUp(desc.Height, UINT(64)) * desc.SampleDesc.Count * dxgiFormatInfo.pixelBytes;
+    return total;
+}
+
 gl::Error Renderer11::createRenderTarget(int width, int height, GLenum format, GLsizei samples, RenderTargetD3D **outRT)
 {
     const d3d11::TextureFormat &formatInfo = d3d11::GetTextureFormatInfo(format, mRenderer11DeviceCaps);
@@ -2873,7 +2888,18 @@ gl::Error Renderer11::createRenderTarget(int width, int height, GLenum format, G
         ASSERT(bindRTV != bindDSV);
 
         ID3D11Texture2D *texture = NULL;
-        HRESULT result = mDevice->CreateTexture2D(&desc, NULL, &texture);
+        HRESULT result;
+
+        // Some Nvidia drivers (GeForce GT 610 w/ 9.18.13.3523) crash with very large render targets
+        if (EstimateSize(desc) > getMaxResourceSize())
+        {
+            result = E_OUTOFMEMORY;
+        }
+        else
+        {
+            result = mDevice->CreateTexture2D(&desc, NULL, &texture);
+        }
+
         if (FAILED(result))
         {
             ASSERT(result == E_OUTOFMEMORY);

DavidSunny86/angle_temp

src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -2786,6 +2786,12 @@ bool Renderer11::resetDevice()
     return true;
 }
 
+SIZE_T Renderer11::getMaxResourceSize() const
+{
+    // This formula comes from http://msdn.microsoft.com/en-us/library/windows/desktop/ff819065%28v=vs.85%29.aspx
+    return std::min(std::max(SIZE_T(128 * 1024 * 1024), mAdapterDescription.DedicatedVideoMemory), SIZE_T(2048) * 1024 * 1024);
+}
+
 std::string Renderer11::getRendererDescription() const
 {
     std::ostringstream rendererString;
@@ -3226,6 +3232,7 @@ gl::Error Renderer11::copyTexture(const gl::Texture *source,
     return gl::NoError();
 }
 
+<<<<<<< HEAD
 gl::Error Renderer11::copyCompressedTexture(const gl::Texture *source,
                                             GLint sourceLevel,
                                             TextureStorage *storage,
@@ -3259,6 +3266,15 @@ gl::Error Renderer11::copyCompressedTexture(const gl::Texture *source,
                                           sourceSubresource, nullptr);
 
     return gl::NoError();
+}
+
+UINT64 EstimateSize(D3D11_TEXTURE2D_DESC &desc)
+{
+    //XXX: handle overflow (64 bits should be enough for anyone...)
+    const d3d11::DXGIFormatSize &dxgiFormatInfo = d3d11::GetDXGIFormatSizeInfo(desc.Format);
+    // NVIDIA seems to align the width of buffers by 8 and the height by 64, so we do the same.
+    UINT64 total = UINT64(rx::roundUp(desc.Width, UINT(8))) * rx::roundUp(desc.Height, UINT(64)) * desc.SampleDesc.Count * dxgiFormatInfo.pixelBytes;
+    return total;
 }
 
 gl::Error Renderer11::createRenderTarget(int width,
@@ -3313,7 +3329,18 @@ gl::Error Renderer11::createRenderTarget(int width,
         ASSERT(bindRTV != bindDSV);
 
         ID3D11Texture2D *texture = NULL;
-        HRESULT result           = mDevice->CreateTexture2D(&desc, NULL, &texture);
+        HRESULT result;
+
+        // Some Nvidia drivers (GeForce GT 610 w/ 9.18.13.3523) crash with very large render targets
+        if (EstimateSize(desc) > getMaxResourceSize())
+        {
+            result = E_OUTOFMEMORY;
+        }
+        else
+        {
+            result = mDevice->CreateTexture2D(&desc, NULL, &texture);
+        }
+
         if (FAILED(result))
         {
             ASSERT(result == E_OUTOFMEMORY);